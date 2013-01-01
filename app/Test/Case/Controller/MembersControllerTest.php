9338c1f3123da8fd73e3b7f4bb7ba2a0b19e656a
pyroka <pyroka@googlemail.com>
2013-01-01 00:32:16 +0000

    Comments, tests and fixture for Pin model. Fixed formatting issues on other fixtures.
    --HG--
    branch : refactor

--- app/Test/Case/Controller/MembersControllerTest.php
+++ app/Test/Case/Controller/MembersControllerTest.php
@@ -2,7 +2,7 @@
 
 	class MembersControllerTest extends ControllerTestCase
 	{
-		public $fixtures = array( 'app.Member', 'app.Status', 'app.Group', 'app.GroupsMember', 'app.Account' );
+		public $fixtures = array( 'app.Member', 'app.Status', 'app.Group', 'app.GroupsMember', 'app.Account', 'app.Pin' );
 
 		public function testIsAuthorized()
 		{
