4b03f682841a130ca099e3e69d27354ef420d3b0
pyroka <pyroka@googlemail.com>
2013-03-09 12:23:11 +0000

    Refactring of AppController
    --HG--
    branch : refactor

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -73,12 +73,12 @@ class AppController extends Controller {
 
         Controller::loadModel('Member');
 
-        $user = $this->Member->find('first', array('conditions' => array( 'Member.member_id' => AuthComponent::user('Member.member_id') )));
-        if( $user )
+        $loggedInMemberId = $this->Member->getIdForMember($this->Auth->user());
+        if($loggedInMemberId)
         {
             $adminLinks = array();
-            if( $this->Member->GroupsMember->isMemberInGroup( Hash::get($user, 'Member.member_id'), Group::FULL_ACCESS ) || 
-                $this->Member->GroupsMember->isMemberInGroup( Hash::get($user, 'Member.member_id'), Group::MEMBER_ADMIN ) )
+            if( $this->Member->GroupsMember->isMemberInGroup( $loggedInMemberId, Group::FULL_ACCESS ) || 
+                $this->Member->GroupsMember->isMemberInGroup( $loggedInMemberId, Group::MEMBER_ADMIN ) )
             {
                 $adminLinks = array(
                     'Members' => array( 'controller' => 'members', 'action' => 'index' ),
@@ -86,37 +86,31 @@ class AppController extends Controller {
                     'Mailing Lists' => array( 'controller' => 'mailinglists', 'action' => 'index' ),
                 );
             }
-            /*else if( Member::isInGroupTourGuide($user) )
-            {
-                $adminLinks = array(
-                    'Add Member' => array( 'controller' => 'members', 'action' => 'add' ),
-                );
-            }*/
 
             $userMessage = array();
-            if( $user['Member']['member_status'] == '5' )
+            if( $this->Member->getStatusForMember($loggedInMemberId) == Status::PRE_MEMBER_2 )
             {
-                $userMessage = array('Click here to enter your contact details!' => array( 'controller' => 'members', 'action' => 'setup_details', $user['Member']['member_id'] ) );
+                $userMessage = array('Click here to enter your contact details!' => array( 'controller' => 'members', 'action' => 'setup_details', $loggedInMemberId ) );
             }
 
             $this->set('adminNav', $adminLinks);
             $this->set('userMessage', $userMessage);
-            $this->set('user', $user);    
+            $this->set('user', $user);
         }
-        
+
         $jsonData = json_decode(file_get_contents('http://lspace.nottinghack.org.uk/status/status.php'));
 
         $this->set('jsonData', $jsonData);
         $this->set('navLinks', $this->Nav->get_allowed_actions());
 
-        $this->set('version', $this->get_version_string());
+        $this->set('version', $this->getVersionString());
     }
 
     public function isAuthorized($user, $request)
     {
         $this->Member = ClassRegistry::init('Member');
 
-        if($this->Member->GroupsMember->isMemberInGroup( Hash::get($user, 'Member.member_id'), Group::FULL_ACCESS ))
+        if($this->Member->GroupsMember->isMemberInGroup( $this->Member->getIdForMember($user), Group::FULL_ACCESS ))
         {
             return true;
         }
@@ -124,7 +118,7 @@ class AppController extends Controller {
         return false;
     }
 
-    public function get_version_string()
+    public function getVersionString()
     {
         return sprintf('%d.%d.%d', self::VERSION_MAJOR, self::VERSION_MINOR, self::VERSION_BUILD);
     }
