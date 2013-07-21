6189a8b932a0737b1fac6120bebbdd478e1520bf
Andrew Thorpe <pyroka@gmail.com>
2013-07-21 10:14:00 +0100

    The 0.3.2 update script now adds the hms_emails table. (Closes #41)

--- dev/setup/lib/updates/0.3.2.php
+++ dev/setup/lib/updates/0.3.2.php
@@ -49,4 +49,13 @@
 
 	$this->_runQuery($conn, $massQuery);
 
+	$this->_logMessage('Creating `hms_emails` table.');
+	$query = "CREATE TABLE IF NOT EXISTS `hms_emails` (
+			  `hms_email_id` int(11) NOT NULL AUTO_INCREMENT,
+			  `member_id` int(11) NOT NULL,
+			  `subject` text NOT NULL,
+			  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			  PRIMARY KEY (`hms_email_id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$this->_runQuery($conn, $query);
 ?>
