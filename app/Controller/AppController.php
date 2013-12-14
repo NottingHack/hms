9baff3e8d760fc31f2cc1aece57178908abf2c0d
Andrew Thorpe <pyroka@gmail.com>
2013-12-14 01:47:00 +0000

    - Fixed documentation issue in AppController.

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -23,12 +23,6 @@ App::uses('AuthComponent', 'Controller/Auth');
 
 /**
  * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @package     app.Controller
- * @link 		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
 
