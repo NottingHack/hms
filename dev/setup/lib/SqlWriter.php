fc40147ab571844ff733a333ad453eb2c6b31bc2
Andrew Thorpe <pyroka@gmail.com>
2013-12-28 16:27:23 +0000

    - Started breaking the code from DataGenerator out in to seperate, testable classes. - Fixed up the phpunit config for dev. - Updated the .gitignore to ignore the results of running the unit tests in dev.

---
+++ dev/setup/lib/SqlWriter.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ *
+ * PHP 5
+ *
+ * Copyright (C) HMS Team
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     HMS Team
+ * @package       dev.Setup.Lib
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+class SqlWriter {
+
+/**
+ * Given a value, return an SQL version of that value. 
+ * @param  mixed $value The value to write.
+ * @return string SQL string representation of the value.
+ */
+	public function write($value) {
+		if (is_numeric($value)) {
+			return strval($value);
+		}
+
+		if (is_string($value)) {
+			// Note that this escaping is not secure
+			// but it's about the best we can do without connecting
+			// to a database.
+			return "'" . str_replace("'", "\'", $value) . "'";
+		}
+
+		return 'NULL';
+	}
+
+/**
+ * Given an name and an array of data, return the SQL code needed
+ * to insert that data into the named table.
+ * @param  string $name Name of the table.
+ * @param  array $data An associative array of data.
+ * @return string|null The SQL code needed to insert data into the table, or null on error.
+ */
+	public function writeTable($name, $data) {
+		if (!is_array($data) || count($data) <= 0) {
+			return null;
+		}
+
+		// Assume that the first 'row' of the data contains all the columns
+		$headers = array_keys($data[0]);
+
+		$sqlHeaders = array_map( function ($val) {
+			return "`$val`";
+		}, $headers);
+
+		$sql = "INSERT INTO `$name` (";
+		$sql .= implode(', ', $sqlHeaders);
+		$sql .= ") VALUES" . PHP_EOL;
+
+		$numItems = count($data);
+		for ($i = 0; $i < $numItems; $i++) {
+			$values = $data[$i];
+
+			$slqValues = array_map( function ($val) {
+				return $this->write($val);
+			}, $values);
+
+			$sql .= "(" . implode(', ', $slqValues) . ")";
+
+			if ($i < $numItems - 1) {
+				$sql .= ',';
+			} else {
+				$sql .= ';';
+			}
+			$sql .= PHP_EOL;
+		}
+
+		return $sql;
+	}
+}
---
+++ dev/tests/Setup/Lib/SqlWriterTest.php
@@ -0,0 +1,118 @@
+<?php
+
+require_once ('Setup/Lib/SqlWriter.php');
+
+class SqlWriterTest extends PHPUnit_Framework_TestCase {
+
+	private $__writer;
+
+	public function setUp() {
+		$this->__writer = new SqlWriter();
+	}
+
+	public function testWrite_WhenPassedInNull_ReturnsSqlNull() {
+		$result = $this->__writer->write(null);
+		$this->assertEquals('NULL', $result);
+	}
+
+	public function testWrite_WhenPassedInPositiveInteger_ReturnsStringOfNumber() {
+		$result = $this->__writer->write(2345436);
+		$this->assertEquals('2345436', $result);
+	}
+
+	public function testWrite_WhenPassedInNumericZero_ReturnsStringZero() {
+		$result = $this->__writer->write(0);
+		$this->assertEquals('0', $result);
+	}
+
+	public function testWrite_WhenPassedInNegativeInteger_ReturnsStringOfNumber() {
+		$result = $this->__writer->write(-253675);
+		$this->assertEquals('-253675', $result);
+	}
+
+	public function testWrite_WhenPassedInPositiveDecimal_ReturnsStringOfNumber() {
+		$result = $this->__writer->write(3.5);
+		$this->assertEquals('3.5', $result);
+	}
+
+	public function testWrite_WhenPassedInDecimalZero_ReturnsStringOfNumber() {
+		$result = $this->__writer->write(0.0);
+		$this->assertEquals('0.0', $result);
+	}
+
+	public function testWrite_WhenPassedInNegativeDecimal_ReturnsStringOfNumber() {
+		$result = $this->__writer->write(-85.64);
+		$this->assertEquals('-85.64', $result);
+	}
+
+	public function testWrite_WhenPassedInEmptyString_ReturnsQuotedEmptyString() {
+		$result = $this->__writer->write('');
+		$this->assertEquals("''", $result);
+	}
+
+	public function testWrite_WhenPassedInStringWithNoApostrophes_ReturnsQuotedString() {
+		$result = $this->__writer->write('the quick brown fox jumps over the lazy dog');
+		$this->assertEquals("'the quick brown fox jumps over the lazy dog'", $result);
+	}
+
+	public function testWrite_WhenPassedInStringWithApostrophe_ReturnsEscapedQuotedString() {
+		$result = $this->__writer->write("you don't want none of this");
+		$this->assertEquals("'you don\'t want none of this'", $result);
+	}
+
+	public function testWriteTable_WhenPassedInEmptyTableNameAndNullData_ReturnsNull() {
+		$result = $this->__writer->writeTable('', null);
+		$this->assertEquals(null, $result);
+	}
+
+	public function testWriteTable_WhenPassedInEmptyTableNameAndEmptyData_ReturnsNull() {
+		$result = $this->__writer->writeTable('', array());
+		$this->assertEquals(null, $result);
+	}
+
+	public function testWriteTable_WhenPassedInEmptyTableNameAndSingleRowOfData_ReturnsSqlForInsertingDataIntoUnnamedTable() {
+		$result = $this->__writer->writeTable('', array(
+			array(
+				'header1' => 'data1',
+				'header2' => 2,
+				'header3' => null,
+			)
+		));
+		$expected = "INSERT INTO `` (`header1`, `header2`, `header3`) VALUES" . PHP_EOL . "('data1', 2, NULL);" . PHP_EOL;
+		$this->assertEquals($expected, $result);
+	}
+
+	public function testWriteTable_WhenPassedInEmptyTableNameAndMultipleRowsOfData_ReturnsSqlForInsertingDataIntoUnnamedTable() {
+		$result = $this->__writer->writeTable('', array(
+			array(
+				'header1' => 'data1',
+				'header2' => 2,
+				'header3' => null,
+			),
+			array(
+				'header1' => 'foo',
+				'header2' => -345,
+				'header3' => 2.1,
+			)
+		));
+		$expected = "INSERT INTO `` (`header1`, `header2`, `header3`) VALUES" . PHP_EOL . "('data1', 2, NULL)," . PHP_EOL . "('foo', -345, 2.1);" . PHP_EOL;
+		$this->assertEquals($expected, $result);
+	}
+
+	public function testWriteTable_WhenPassedInTableNameAndMultipleRowsOfData_ReturnsSqlForInsertingDataIntoUnnamedTable() {
+		$result = $this->__writer->writeTable('tableName', array(
+			array(
+				'header1' => 'data1',
+				'header2' => 2,
+				'header3' => null,
+			),
+			array(
+				'header1' => 'foo',
+				'header2' => -345,
+				'header3' => 2.1,
+			)
+		));
+		$expected = "INSERT INTO `tableName` (`header1`, `header2`, `header3`) VALUES" . PHP_EOL . "('data1', 2, NULL)," . PHP_EOL . "('foo', -345, 2.1);" . PHP_EOL;
+		$this->assertEquals($expected, $result);
+	}
+}
