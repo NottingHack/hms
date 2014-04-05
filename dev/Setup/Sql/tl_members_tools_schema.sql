75cf994d1bd0f8ffb450d29c29509a1b40dae926
James Hayward <jhayward1980@gmail.com>
2014-04-05 22:35:23 +0100

    Adding SQL files for tools to setup

---
+++ dev/Setup/Sql/tl_members_tools_schema.sql
@@ -0,0 +1,11 @@
+create table tl_members_tools
+(
+  member_tool_id    int not null auto_increment,
+  member_id         int,
+  tool_id           int,
+  member_id_induct  int,         -- member_id of inductor
+  mt_date_inducted  datetime,
+  mt_access_level   varchar(20), -- USER, INDUCTOR or MAINTAINER
+  primary key (member_tool_id),
+  unique (tool_id, member_id)
+);
