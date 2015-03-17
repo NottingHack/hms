d890e2bbfa32c909ce95f692da83c6e9d0692311
dpslwk <dps.lwk@gmail.com>
2015-03-17 21:53:51 +0000

    BankTransactionFixture test data, need to generate more at some point

--- app/Test/Fixture/BankTransactionFixture.php
+++ app/Test/Fixture/BankTransactionFixture.php
@@ -7,6 +7,10 @@
 //        public $tabel = 'bank_transactions';
 //        public $primaryKey = 'bank_transaction_id';	
 		public $records = array(
+            array('bank_transaction_id' => '1','date' => '2015-03-02','description' => 'Mathew Pryce, HSNOTTS6762KC8JD','amount' => '10','bank_id' => '1','account_id' => '1'),
+            array('bank_transaction_id' => '2','date' => '2015-02-02','description' => 'Mathew Pryce, HSNOTTS6762KC8JD','amount' => '10','bank_id' => '1','account_id' => '1'),
+            array('bank_transaction_id' => '3','date' => '2015-03-04','description' => 'Mathew Pryce HSNOTTS6762KC8JD','amount' => '10','bank_id' => '2','account_id' => '1'),
+            array('bank_transaction_id' => '4','date' => '2015-03-02','description' => '5000001','amount' => '371','bank_id' => '1','account_id' => null)
         );
 	}
 
