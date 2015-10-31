0db43cddeded50eab4d4781af5155f593df09ca9
dps.lwk <dps.lwk@gmail.com>
2015-10-31 16:18:20 +0000

    update schema to add Natwest ref column

--- dev/Setup/Sql/account_schema.sql
+++ dev/Setup/Sql/account_schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `account` (
   `account_id` int(11) NOT NULL AUTO_INCREMENT,
   `payment_ref` varchar(18) NOT NULL,
+  `natwest_ref` varchar(18) NOT NULL,
   PRIMARY KEY (`account_id`),
   UNIQUE KEY `payment_ref` (`payment_ref`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
