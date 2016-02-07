fe3a4693bd0169ab6cd4334868cd748bfcdb5d86
dpslwk <dps.lwk@gmail.com>
2016-02-07 13:22:30 +0000

    Update dataGenerator to make correct timestamps for raid tags

--- app/Test/Fixture/RfidTagFixture.php
+++ app/Test/Fixture/RfidTagFixture.php
@@ -6,13 +6,13 @@
 		public $import = 'RfidTag';
 
 		public $records = array(
-			array('member_id' => 1, 'rfid_serial' => '103162154', 'state' => '10', 'last_used' => '1454817855', 'friendly_name' => 'White one'),
-			array('member_id' => 2, 'rfid_serial' => '1727513448', 'state' => '10', 'last_used' => '1454817855', 'friendly_name' => 'Mango'),
-            array('member_id' => 3, 'rfid_serial' => '178100790', 'state' => '10', 'last_used' => '1454817855', 'friendly_name' => ''),
-            array('member_id' => 4, 'rfid_serial' => '1846700113', 'state' => '10', 'last_used' => '1454817856', 'friendly_name' => 'White one'),
-            array('member_id' => 5, 'rfid_serial' => '158317848', 'state' => '20', 'last_used' => '1454817455', 'friendly_name' => 'Lost'),
-            array('member_id' => 6, 'rfid_serial' => '1740899925', 'state' => '10', 'last_used' => '1454817255', 'friendly_name' => ''),
-            array('member_id' => 7, 'rfid_serial' => '105994420', 'state' => '10', 'last_used' => '1454810055', 'friendly_name' => 'Oyster'),
+			array('member_id' => 1, 'rfid_serial' => '103162154', 'state' => '10', 'last_used' => '2015-08-21 23:36:46', 'friendly_name' => 'White one'),
+			array('member_id' => 2, 'rfid_serial' => '1727513448', 'state' => '10', 'last_used' => '2015-12-10 00:48:41', 'friendly_name' => 'Mango'),
+            array('member_id' => 3, 'rfid_serial' => '178100790', 'state' => '10', 'last_used' => '2015-12-19 01:32:14', 'friendly_name' => ''),
+            array('member_id' => 4, 'rfid_serial' => '1846700113', 'state' => '10', 'last_used' => '2015-11-28 01:48:45', 'friendly_name' => 'White one'),
+            array('member_id' => 4, 'rfid_serial' => '158317848', 'state' => '20', 'last_used' => '2016-01-14 11:27:42', 'friendly_name' => 'Lost'),
+            array('member_id' => 6, 'rfid_serial' => '1740899925', 'state' => '10', 'last_used' => '', 'friendly_name' => ''),
+            array('member_id' => 6, 'rfid_serial' => '105994420', 'state' => '10', 'last_used' => '', 'friendly_name' => 'Oyster'),
 		);
 	}
 
