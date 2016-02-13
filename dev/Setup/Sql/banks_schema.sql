5edf17d7408539aadbbccbe690d4fed55bd919bc
dpslwk <dps.lwk@gmail.com>
2016-02-13 13:12:21 +0000

    Amend Banks table to include sort code and account number for future use

--- dev/Setup/Sql/banks_schema.sql
+++ dev/Setup/Sql/banks_schema.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS `banks` (
  `bank_id` int(11) NOT NULL AUTO_INCREMENT,
  `name` varchar(100) NOT NULL,
+ `sort_code` varchar(8) DEFUALT NULL,
+ `account_number` varchar(8) DEFUALT NULL,
  PRIMARY KEY (`bank_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
--- dev/Setup/Updates/0.5.0.php
+++ dev/Setup/Updates/0.5.0.php
@@ -33,6 +33,8 @@ $this->__logMessage('Creating `banks` table.');
 $query = "CREATE TABLE IF NOT EXISTS `banks` (
            `bank_id` int(11) NOT NULL AUTO_INCREMENT,
            `name` varchar(100) NOT NULL,
+           `sort_code` varchar(8) DEFUALT NULL,
+           `account_number` varchar(8) DEFUALT NULL,
            PRIMARY KEY (`bank_id`)
           ) ENGINE=InnoDB DEFAULT CHARSET=latin1";
 $this->__runQuery($conn, $query);
