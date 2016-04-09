3346e4962af6c8aa887c4598be18cebe3c8ba1ec
dpslwk <dps.lwk@gmail.com>
2016-04-09 18:45:25 +0100

    MemberBox: Add recordTransaction to transaction controller

--- app/Model/Transactions.php
+++ app/Model/Transactions.php
@@ -26,7 +26,10 @@ class Transactions extends AppModel {
 
 	const TYPE_VEND = "VEND";			// Transaction relates to either vending machine purchace, or a payment received by note acceptor
 	const TYPE_MANUAL = "MANUAL";	// Transaction is a manually entered (via web interface) record of a payment or purchase
-
+    const TYPE_TOOL = "TOOL";
+    const TYPE_MEMBERBOX = "MEMBERBOX";
+    const STATE_COMPLETE = "COMPLETE";
+    const STATE_PENDING = "PENDING";
 /**
  * Specify the table to use.
  * @var string
@@ -54,7 +57,7 @@ class Transactions extends AppModel {
 /**
  * Get a list of transations for a member
  * 
- * @param $paginate If true, return a query to retrieve a page of the data, otherwise return the data.
+ * @param bool $paginate If true, return a query to retrieve a page of the data, otherwise return the data.
  * @param array $conditions An array of conditions to decide which member records to access.
  * @return array A list of transactions or query to reports a list of transactions
  */
@@ -73,4 +76,69 @@ class Transactions extends AppModel {
 		return $info;
 	}
 
+/**
+ * Record a new transaction for a member
+ *
+ * @param int $memberId
+ * @param int $ammount in pence, negative is charge, postive is credit
+ * @param string $type
+ * @param string $description
+ * @param int $recordedBy person who recorded the transaction, can be null if same as $memberID
+ * @return bool
+ */
+    public function recordTransaction($memberId, $ammount, $type, $description, $recordedBy = null) {
+        // check member is not null
+        if ($memberId == null) {
+            return false;
+        }
+        
+        // check amount is number
+        if (!isNumeric($amount)) {
+            return false;
+        }
+
+        // if recordedBy
+        if ($memberId == $recordedBy) {
+            $recordedBy = null;
+        }
+        
+        // grab member and double check credit
+        $member = $this->Member->getMemberSummaryForMember($memberId, false);
+        if (($member['Member']['balance'] + $ammount) < (-1 *$member['creditLimit'])) {
+            return false;
+        }
+        
+        // prep records
+        $transaction = array(
+                             'Transaction' => array(
+                                                    'member_id' => $memberId,
+                                                    'ammount' => $ammount,
+                                                    'transaction_type' => $type,
+                                                    'transaction_status' => Transaction::STATE_COMPLETE,
+                                                    'transaction_desc' => $description,
+                                                    'recorded_by' => $recordedBy,
+                                                    )
+        
+                            );
+        
+        $dataSource = $this->getDataSource();
+        $dataSource->begin();
+        
+        // create new transaction
+        $this->create();
+        
+        if(!$this->save($transaction)) {
+            $dataSource->rollback();
+        }
+        
+        // update memberBalance
+        if (!$this->Member->updateBalanceForMember($member, $ammount)) {
+            $dataSource->rollback();
+            return false;
+        }
+        
+        $dataSource->commit();
+        return true;
+        
+    }
 }
